5e32dd2010fc5654bc1a989a2af8c1b40f860157
bekreth <bekreth@gmail.com>
2020-02-19 09:39:00 -0500

    Hello loops

--- combining_caribou_teeth.R
+++ combining_caribou_teeth.R
@@ -1,17 +1,25 @@
-sample01A <- read.csv("01A.csv", header = FALSE)
-colnames(sample01A) <- c("time", "Sr")
 
-sample01B <- read.csv("01B.csv", header = FALSE)
-colnames(sample01B) <- c("time", "Sr")
+sampler <- function(fileNumber, fileCharacter) {
+  fileName <- sprintf("%02d%s.csv", fileNumber, fileCharacter)
+  print(fileName)
+  sample <- read.csv(fileName)
+  colnames(sample) <- c("time", "Sr")
+  return(sample)
+}
 
-sample01C <- read.csv("01C.csv", header = FALSE)
-colnames(sample01C) <- c("time", "Sr")
+save_as_png = function(plot_maker, filename) {
+  png(sprintf("%s/%s.png", outputDirectory, filename))
+  plot_maker(data)
+  dev.off()
+}
 
-sample01 <- rbind(sample01A, sample01B, sample01C)
-plot(sample01, type = "p", pch = 20, cex = 0.5)
+for (fileCount in 1:15) {
+  sampleA <- sampler(fileCount, 'A')
+  sampleB <- sampler(fileCount, 'B')
+  sampleC <- sampler(fileCount, 'C')
 
-
-#Each sample has 3 parts (A, B, and C), and I want to plot them together as done above.
-#I would like to figure out a way to make this more efficient, perhaps by making a function
-#that reads every csv in the folder at once, combines the ones that have the same numerical
-#part of their name using rbind, and then plots them.
+  png(sprintf("%02d.png", fileCount))
+  sample01 <- rbind(sampleA, sampleB, sampleC)
+  plot(sample01, type = "p", pch = 20, cex = 0.5)
+  dev.off()
+}
