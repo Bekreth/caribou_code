a8fa836427775f769ccfe21c00855a2d1fb6bdd8
bekreth <bekreth@gmail.com>
2020-02-19 09:40:53 -0500

    Cleaned up logging

--- combining_caribou_teeth.R
+++ combining_caribou_teeth.R
@@ -1,17 +1,11 @@
 
 sampler <- function(fileNumber, fileCharacter) {
   fileName <- sprintf("%02d%s.csv", fileNumber, fileCharacter)
-  print(fileName)
   sample <- read.csv(fileName)
   colnames(sample) <- c("time", "Sr")
   return(sample)
 }
 
-save_as_png = function(plot_maker, filename) {
-  png(sprintf("%s/%s.png", outputDirectory, filename))
-  plot_maker(data)
-  dev.off()
-}
 
 for (fileCount in 1:15) {
   sampleA <- sampler(fileCount, 'A')
@@ -22,4 +16,5 @@ for (fileCount in 1:15) {
   sample01 <- rbind(sampleA, sampleB, sampleC)
   plot(sample01, type = "p", pch = 20, cex = 0.5)
   dev.off()
+
 }
